6577cda724810aef3bace829f1fd0474f1e8c0ef
Ya! Gamer Chris Pages <christopher.celaya.ric2022@gmail.com>
2025-12-08 10:53:01 -0600

    Update jsforPDP.js

--- Complements/jsforPDP.js
+++ Complements/jsforPDP.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js")
+  navigator.serviceWorker.register("../AppWeb/sw.js")
     .then(() => console.log("✅ Service Worker registrado correctamente"))
     .catch((err) => console.error("❌ Error al registrar el Service Worker:", err));
 }
