f9d97e49be7b08e0dc0b43af87ec50d5089e0d11
Ya! Gamer Chris Pages <christopher.celaya.ric2022@gmail.com>
2025-12-11 13:23:10 -0600

    Delete Complements/jsforPDP.js

--- Complements/jsforPDP.js
+++
@@ -1,5 +0,0 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("../AppWeb/sw.js")
-    .then(() => console.log("✅ Service Worker registrado correctamente"))
-    .catch((err) => console.error("❌ Error al registrar el Service Worker:", err));
-}
